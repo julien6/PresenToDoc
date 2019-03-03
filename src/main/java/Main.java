26990d81c99ace3c840135fa49f6ef9ac6111993
julien6 <julien.soule.1@etsmtl.net>
2019-03-03 15:34:52 -0500

    initial commit.v2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,20 @@
+import org.apache.pdfbox.pdmodel.PDDocument;
+
+import java.io.IOException;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("ok");
+        PDDocument document = new PDDocument();
+
+        try {
+            document.save("output.pdf");
+            document.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 
 }
