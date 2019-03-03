f57c1e2177587cef44cfa0504503f0efd534dcc6
julien6 <julien.soule.1@etsmtl.net>
2019-03-03 15:54:30 -0500

    definition v1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,36 @@
 import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
 
+import java.io.File;
 import java.io.IOException;
+
+// https://www.tutorialspoint.com/pdfbox/pdfbox_document_properties.htm
+
+// TODO : Find main goals of PresenToDoc :
+//  - save, load,
+//  - reorganize information
+//      - extract text,
+//      - extract picture
+//      - several choices of organisation (text and pictures two sided, find ways to keep main organisation)
+//          - extract structural and comestical informations
 
 public class Main {
 
     public static void main(String[] args) {
 
         PDDocument document = new PDDocument();
+        PDPage page = new PDPage();
+        File file = new File("pdfDocument.pdf");
+
+        System.out.println("ok");
+        System.out.println("ok");
 
         try {
             document.save("output.pdf");
+            document.addPage(page);
+            PDDocument doc2 = PDDocument.load(file);
+            doc2.save("second.pdf");
+            doc2.close();
             document.close();
         } catch (IOException e) {
             e.printStackTrace();
